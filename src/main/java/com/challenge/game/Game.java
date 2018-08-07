b530970177a62fafb40af7686eb7ff5cb4cfc972
Cristian Dugacicu <razorcd@yahoo.com>
2018-08-07 16:59:17 +0200

    document game methods

--- src/main/java/com/challenge/game/Game.java
+++ src/main/java/com/challenge/game/Game.java
@@ -6,11 +6,19 @@ public class Game {
 
     private InputNumber inputNumber;
 
+    /**
+     * Initialise game with input.
+     * @param inputNumber the input number value object.
+     */
     public Game(InputNumber inputNumber) {
         if (!inputNumber.isValid()) throw new InputMismatchException("input is invalid");
         this.inputNumber = inputNumber;
     }
 
+    /**
+     * Get output value of the game.
+     * @return [int] the output value
+     */
     public int getOutput() {
         int modulo = inputNumber.getValue()%3;
         int[] additions = {0, -1, 1};
