fd5d60e77e8dfeb2fb273439d32c235378abe8b6
Cristian Dugacicu <razorcd@yahoo.com>
2018-08-07 16:14:23 +0200

    init mvn project

---
+++ src/main/java/com/challenge/game/App.java
@@ -0,0 +1,9 @@
+package com.challenge.game;
+
+public class App {
+    public static void main( String[] args ) {
+        
+
+
+    }
+}
---
+++ src/test/java/com/challenge/game/AppTest.java
@@ -0,0 +1,12 @@
+package com.challenge.game;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue( true );
+    }
+}
